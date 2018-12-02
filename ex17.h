baf25f95d8933513859d81cbaaa598e47bdf1624
Iago Gade Gusmao Carrazzoni <iagocarrazzoni@gmail.com>
2018-12-02 20:50:39 -0300

    ex17.h adicionado

---
+++ ex17.h
@@ -0,0 +1,138 @@
+/***************************************************************************
+ *   ex17.h                                   Version 20181202.205013      *
+ *                                                                         *
+ *                                                                         *
+ *   Copyright (C) 2018         by Iago Gade Gusmao Carrazzoni             *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Iago Gade Gusmao Carrazzoni                                           *
+ *   Email: iagocarrazzoni@gmail.com                                       *
+ *   Webpage: www.upe.br                                                   *
+ *   Phone: +55 (81) 99479-0584                                            *
+ ***************************************************************************/
+
+/* ---------------------------------------------------------------------- */
+/**
+ * \file ex17.h
+ * \ingroup GroupUnique
+ * \brief 
+ * \details This program really do a nice job as a template, and template only!
+ * \version 20181202.205013
+ * \date 2018-12-02
+ * \author Iago Gade Gusmao Carrazzoni <<iagocarrazzoni@gmail.com>>
+ * \par Webpage
+ * <<a href="www.upe.br">www.upe.br</a>>
+ * \copyright (c) 2018 GNU GPL v2
+ * \note This program is free software: you can redistribute it
+ * and/or modify it under the terms of the
+ * GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ * If not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA. 02111-1307, USA.
+ * Or read it online at <<http://www.gnu.org/licenses/>>.
+ *
+ *
+ * \todo Now that you have the template, hands on! Programme!
+ * \warning Be carefull not to lose your mind in small things.
+ * \bug This file right now does nothing usefull
+ *
+ */
+
+#ifndef _EX17_H
+#define _EX17_H
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+
+/* #include <stdio.h> */ /* Standard I/O functions */
+/* #include <stdlib.h> */ /* Miscellaneous functions (rand, malloc, srand)*/
+/* #include <getopt.h> */ /* get options from system argc/argv */
+
+/* #include <time.h> */ /* Time and date functions */
+/* #include <math.h> */ /* Mathematics functions */
+/* #include <string.h> */ /* Strings functions definitions */
+/* #include <dlfcn.h> */ /* Dynamic library */
+/* #include <malloc.h> */ /* Dynamic memory allocation */
+/* #include <unistd.h> */ /* UNIX standard function */
+/* #include <limits.h> */ /* Various C limits */
+/* #include <ctype.h> */ /* Character functions */
+/* #include <errno.h> */ /* Error number codes errno */
+/* #include <signal.h> */ /* Signal processing */
+/* #include <stdarg.h> */ /* Functions with variable arguments */
+/* #include <pthread.h> */ /* Parallel programming with threads */
+/* #include <fcntl.h> */ /* File control definitions */
+/* #include <termios.h> */ /* Terminal I/O definitions POSIX */
+/* #include <sys/stat.h> */ /* File status and information */
+/* #include <float.h> */ /* Float constants to help portability */
+/* #include <setjmp.h> */ /* Bypass standard function calls and return */
+/* #include <stddef.h> */ /* Various types and MACROS */
+/* #include <SWI-Prolog.h> */ /* Prolog integration with C */
+/* #include <ncurses.h> */ /* Screen handling and optimisation functions */
+/* #include <allegro.h> */ /* A game library for graphics, sounds, etc. */
+/* #include <libintl.h> */ /* Internationalization / translation */
+/* #include <locale.h> */ /* MACROS LC_ for location specific settings */
+/* #include "libeco.h" */ /* I/O, Math, Sound, Color, Portable Linux/Windows */
+/* #include "ex17.h" */ /* To be created for this template if needed */
+
+/* ---------------------------------------------------------------------- */
+/* definitions */
+
+#ifndef VERSION /* gcc -DVERSION="0.1.160612.142628" */
+#define VERSION "20181202.205013" /**< Version Number (string) */
+#endif
+
+/* Debug */
+#ifndef DEBUG /* gcc -DDEBUG=1 */
+#define DEBUG 0 /**< Activate/deactivate debug mode */
+#endif
+
+#if DEBUG==0
+#define NDEBUG
+#endif
+/* #include <assert.h> */ /* Verify assumptions with assert. Turn off with #define NDEBUG */ 
+
+/** @brief Debug message if DEBUG on */
+#define IFDEBUG(M) if(DEBUG) fprintf(stderr, "[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
+
+/* limits */
+#define SBUFF 256 /**< String buffer */
+
+/* ---------------------------------------------------------------------- */
+/* globals */
+
+static int verb = 0; /**< verbose level, global within the file */
+
+/* ---------------------------------------------------------------------- */
+/* prototypes */
+
+void help(void); /**< Prints help information and exit */
+void copyr(void); /**< Prints copyright information and exit */
+void ex17_init(void);  /**< Initializes some operations before start */
+void ex17_update(void); /**< Updates values during program cycle */
+
+#endif /* NOT def _EX17_H */
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160615.020326      */
+
